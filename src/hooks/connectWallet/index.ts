61113cce21d37b25df323cae49225088deb23836
Alex Santos <alexdsantosv@gmail.com>
2022-06-17 15:55:32 -0600

    [ Fix ] open wallet

--- src/hooks/connectWallet/index.ts
+++ src/hooks/connectWallet/index.ts
@@ -10,12 +10,14 @@ import { useWeb3Modal } from "../../providers/Web3/hooks";
 const useConnectWalletBtn = () => {
   // CONTEXT
   const { address, connectWallet, disconnectWallet } = useWeb3Modal();
+  const path = window.location.pathname;
 
   useEffect(() => {
     // FIND BUTTON
     const button = document.querySelector(
-      "a.action_button[href='#CONNECTWALLET']"
+      "a.action_button[href='https://abscissalabs.xyz/connect-wallet/']"
     );
+
     const event = () => {
       if (!address.length) {
         if (connectWallet) connectWallet();
@@ -24,12 +26,16 @@ const useConnectWalletBtn = () => {
       }
     };
 
-    // SET BUTTON EVENTS AND TEXT
-    if (button) {
-      (button as HTMLAnchorElement).addEventListener("click", event);
-      button!.textContent = address.length
-        ? `${address.substring(0, 5)} ... ${address.substring(5, 10)}`
-        : "CONNECT WALLET";
+    if (path.startsWith("/connect-wallet")) {
+      // SET BUTTON EVENTS AND TEXT
+      if (button) {
+        (button as HTMLAnchorElement).href =
+          "https://abscissalabs.xyz/connect-wallet/#open";
+        (button as HTMLAnchorElement).addEventListener("click", event);
+        button!.textContent = address.length
+          ? `${address.substring(0, 5)} ... ${address.substring(5, 10)}`
+          : "CONNECT WALLET";
+      }
     }
 
     // REMOVE LISTENERS
